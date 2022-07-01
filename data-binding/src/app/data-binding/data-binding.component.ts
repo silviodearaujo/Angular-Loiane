3a75211f8e902e81b7d83f1a8420499d6d77c308
Silvio Cesar Araujo <araujo.scesar@gmail.com>
2022-06-30 22:09:34 -0300

    Event Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,17 +9,38 @@ export class DataBindingComponent implements OnInit {
 
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
-  urlImagem = 'http://lorempixel.com.br/400/200/?4'
+  urlImagem = 'http://lorempixel.com.br/400/200/?4';
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+  
 
   //além de expressoes matematicas, expressoes booleanas
   //nesse caso a função abaixo nao precisa ser iniciada com function e nem terminada com ; 
-  getValor(){
+  getValor() {
     return 1
   }
 
-    getCurtirCurso(){
-      return true
-    }
+  salvarValor(valor:any){
+    this.valorSalvo = valor;
+  }
+
+  getCurtirCurso() {
+    return true
+  }
+  botaoClicado() {
+    window.alert('Botão clicado!');
+  }
+  onKeyUp(evento: KeyboardEvent) {
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  } 
 
   constructor() { }
 
